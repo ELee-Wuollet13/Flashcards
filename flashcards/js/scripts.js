29983edc8a472e6e83dda73fecb38321a163478c
Student <example@example.com>
2019-10-22 14:43:59 -0700

    add toggle to all cards

--- flashcards/js/scripts.js
+++ flashcards/js/scripts.js
@@ -1,7 +1,38 @@
 $(document).ready(function() {
-    $(".Methods").click(function() {
+    // $(".methods").click(function() {
+    //   console.log("Hello");
+    //   $(".back").toggle();
+    //   $(".front").toggle();
+    // });
+    $(".javascript").click(function() {
+      console.log("Hello");
+      $(".back").toggle();
+      $(".front").toggle();
+    });
+    $(".operators").click(function() {
+      console.log("Hello");
+      $(".back").toggle();
+      $(".front").toggle();
+    });
+    $(".variables").click(function() {
+      console.log("Hello");
+      $(".back").toggle();
+      $(".front").toggle();
+    });
+    $(".naming").click(function() {
+      console.log("Hello");
+      $(".back").toggle();
+      $(".front").toggle();
+    });
+    $(".functions").click(function() {
+      console.log("Hello");
+      $(".back").toggle();
+      $(".front").toggle();
+    });
+    $(".methods").click(function() {
       console.log("Hello");
       $(".back").toggle();
       $(".front").toggle();
     });
 });
+// });
